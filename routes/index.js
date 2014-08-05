4b7bb9f1c7da7de17413b4b6ff61d69aed743446
Lisa Ha <lisamayha@gmail.com>
2014-08-04 21:20:46 -0700

    when a new client joins, they can see all the drawings that were drawn prior to connection

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,29 @@
 module.exports = function Route(app) {
-	app.io.route('drawClick', function(req) {
-		req.io.broadcast('draw', req.data);
-	})
+	var drawings = {};
+	var drawing = [];
+	var counter = 0;
+
 	app.get('/', function(req, res) {
 		res.render('client')
+	})
+
+	app.io.route('connection', function(req) {
+		req.io.emit('old_drawings', drawings);
+	})
+
+	app.io.route('drawClick', function(req) {
+		drawing.push(req.data);
+		req.io.broadcast('draw', req.data);
+		if(req.data.type == 'dragend') {
+			drawings[counter] = drawing;
+			drawing = [];
+			counter++;
+		}
+	})
+
+	app.io.route('erase_whitebaord', function(req) {
+		drawings = {};
+		counter = 0;
+		app.io.broadcast('new_whiteboard');
 	})
 }
